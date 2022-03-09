949ad082f4faf65a331aca6a17e0b5199e04a2e9
thaothaok15 <thaohtthe153475@fpt.edu.vn>
2022-03-10 01:01:20 +0700

    search controller 2

--- Cosmetic/src/java/controller/SearchCotroller.java
+++ Cosmetic/src/java/controller/SearchCotroller.java
@@ -33,12 +33,16 @@ public class SearchCotroller extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
+         request.setCharacterEncoding("UTF-8");
        String txtSearch = request.getParameter("txt");
+      
        DAO dao = new DAO();
        
        List<Product> list = dao.searchByName(txtSearch);
        
+       
        request.setAttribute("listP", list);
+       request.setAttribute("txtS", txtSearch);
        request.getRequestDispatcher("Home.jsp").forward(request, response);
     }
 
